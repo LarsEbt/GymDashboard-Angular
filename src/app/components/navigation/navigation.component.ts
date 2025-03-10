df1a2a896f68248659ec89f2f98817bc969b112a
LarsEbt <larsebert0104@gmail.com>
2025-03-10 17:50:19 +0100

    build: first Sync

---
+++ src/app/components/navigation/navigation.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation',
+  imports: [],
+  templateUrl: './navigation.component.html',
+  styleUrl: './navigation.component.css'
+})
+export class NavigationComponent {
+
+}
+
+/*<span class="material-symbols-outlined">
+dashboard
+</span>*/ 
+
+
+/*<span class="material-symbols-outlined">
+inventory_2
+</span>*/
+
+/*<span class="material-symbols-outlined">
+settings
+</span>*/
+
